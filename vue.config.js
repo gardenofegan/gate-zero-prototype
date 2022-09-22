2888e7714268f3bd22c8f54a8d91b8b2ab28fa6f
Justin Egan <justin.egan@ge.com>
2022-09-22 03:20:00 +0100

    Generate /docs folder

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: '',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/gate-zero-prototype/'
+    : '/',
   outputDir: 'docs',
   transpileDependencies: [
     'vuetify'
